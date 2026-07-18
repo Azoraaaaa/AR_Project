876d5a0d4891b4141547121190f347645628dd63
Shao-Qiuting <mec2209490@xmu.edu.my>
2026-07-18 16:23:13 +0800

    NextPage

--- Assets/Scripts/SQT/DragItem.cs
+++ Assets/Scripts/SQT/DragItem.cs
@@ -5,34 +5,83 @@ using UnityEngine.UI;
 public class DragItem : MonoBehaviour,
 IBeginDragHandler, IDragHandler, IEndDragHandler
 {
+    [Header("Card ID")]
     public int cardID;
+
+
+    [Header("Drag Sound")]
+    public AudioSource audioSource;
+    public AudioClip dragSound;
+
+
 
     private Vector2 startPos;
 
+
+
     void Start()
     {
-        startPos = GetComponent<RectTransform>().anchoredPosition;
+        startPos =
+            GetComponent<RectTransform>()
+            .anchoredPosition;
     }
+
+
+
 
 
     public void OnBeginDrag(PointerEventData eventData)
     {
+        // 播放拖动音效
+        if (audioSource != null &&
+           dragSound != null)
+        {
+            audioSource.PlayOneShot(dragSound);
+        }
+
+
+
         // 拖动时允许穿透检测
-        GetComponent<CanvasGroup>().blocksRaycasts = false;
+        CanvasGroup canvasGroup =
+            GetComponent<CanvasGroup>();
+
+        if (canvasGroup != null)
+        {
+            canvasGroup.blocksRaycasts = false;
+        }
     }
+
+
+
+
 
 
     public void OnDrag(PointerEventData eventData)
     {
-        transform.position += (Vector3)eventData.delta;
+        transform.position +=
+            (Vector3)eventData.delta;
     }
+
+
+
+
 
 
     public void OnEndDrag(PointerEventData eventData)
     {
-        GetComponent<CanvasGroup>().blocksRaycasts = true;
+        CanvasGroup canvasGroup =
+            GetComponent<CanvasGroup>();
+
+
+        if (canvasGroup != null)
+        {
+            canvasGroup.blocksRaycasts = true;
+        }
+
+
 
         // 如果没有成功放置，回原位置
-        GetComponent<RectTransform>().anchoredPosition = startPos;
+        GetComponent<RectTransform>()
+            .anchoredPosition = startPos;
     }
 }
